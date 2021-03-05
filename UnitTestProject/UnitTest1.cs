c11a8f1fa04419169c7c7d6e81ea370c87abcc59
sandrobtn <lescole.alessandro@gmail.com>
2021-03-05 16:39:31 +0100

    TestUnitairesAPI

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScanDemo.Models;
+using ScanDemo.Repositories;
+using ScanDemo.Services;
+using ScanDemo.ViewModels;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void MockAddItemAsync()
+        {
+            // Arrange
+            MockDataStore mock = new MockDataStore();
+            Item item = new Item { Id = "999", Text = "First item", Description = "This is an item description." };
+
+            // Act
+            var result = mock.AddItemAsync(item).Result;
+
+            // Assert
+            if (result != true)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void MockUpdateItemAsync()
+        {
+            // Arrange
+            MockDataStore mock = new MockDataStore();
+            Item item = new Item { Id = "999", Text = "Other item", Description = "This is an other item description." };
+
+            // Act
+            var result = mock.UpdateItemAsync(item).Result;
+
+            // Assert
+            if (result != true)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void MockDeleteItemAsync()
+        {
+            // Arrange
+            MockDataStore mock = new MockDataStore();
+
+            // Act avec id = "1"
+            var result = mock.DeleteItemAsync("999").Result;
+
+            // Assert
+            if (result != true)
+            {
+                Assert.Fail();
+            }
+        }
+        
+        [TestMethod]
+        public void MockGetItemAsync()
+        {
+            // Arrange
+            MockDataStore mock = new MockDataStore();
+            Item item = new Item { Id = "666", Text = "First item", Description = "This is an item description." };
+            mock.AddItemAsync(item);
+
+            // Act avec id = "1"
+            var result = mock.GetItemAsync("666").Result;
+
+            // Assert
+            if (result.Id != "666")
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
